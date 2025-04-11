670e9879cba85475b8f18c4d7fdb08c930c6e7b8
Naruto777549649 <sikrar026@gmail.com>
2025-04-11 15:34:32 +0530

    Update App.jsx

--- public/App.jsx
+++ public/App.jsx
@@ -1,1 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import AnimeDetails from './pages/AnimeDetails';
+import Episode from './pages/Episode';
+import WatchList from './pages/WatchList';
 
+const App = () => {
+  return (
+    <Router>
+      <div className="flex flex-col min-h-screen bg-gray-900 text-white">
+        <Navbar />
+        <main className="flex-1">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/anime/:id" element={<AnimeDetails />} />
+            <Route path="/episode/:id" element={<Episode />} />
+            <Route path="/watchlist" element={<WatchList />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+};
+
+export default App;
