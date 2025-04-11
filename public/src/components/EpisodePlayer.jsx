1669a6dbe8afe15cfa359cf15a85dfb38bb6c58c
Naruto777549649 <sikrar026@gmail.com>
2025-04-11 15:37:51 +0530

    Update EpisodePlayer.jsx

--- public/src/components/EpisodePlayer.jsx
+++ public/src/components/EpisodePlayer.jsx
@@ -1,1 +1,19 @@
+// components/EpisodePlayer.jsx
+import React from 'react';
 
+const EpisodePlayer = ({ title, videoUrl }) => {
+  return (
+    <div className="w-full aspect-video bg-black rounded-lg overflow-hidden shadow-lg">
+      <iframe
+        className="w-full h-full"
+        src={videoUrl}
+        title={title}
+        allowFullScreen
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        frameBorder="0"
+      ></iframe>
+    </div>
+  );
+};
+
+export default EpisodePlayer;
